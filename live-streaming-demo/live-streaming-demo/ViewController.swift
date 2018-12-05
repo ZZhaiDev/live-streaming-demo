8ba3f07be1efe2c247bd6e1a8ae2461ebef8c9b0
zijia <zijia@zijiadeiMac.local>
2018-12-04 20:18:19 -0600

    init push

---
+++ live-streaming-demo/live-streaming-demo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  live-streaming-demo
+//
+//  Created by zijia on 12/4/18.
+//  Copyright © 2018 zijia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
