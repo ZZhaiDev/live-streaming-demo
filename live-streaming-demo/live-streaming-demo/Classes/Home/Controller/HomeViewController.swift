7d397aa2bb624a3fe73a7979980197bff98628de
zijia <zijia@zijiadeiMac.local>
2018-12-05 21:35:38 -0600

    PageTitleFView Done

--- live-streaming-demo/live-streaming-demo/Classes/Home/Controller/HomeViewController.swift
+++ live-streaming-demo/live-streaming-demo/Classes/Home/Controller/HomeViewController.swift
@@ -8,19 +8,31 @@
 
 import UIKit
 
+private let kTitleViewH : CGFloat = 40
+
 class HomeViewController: UIViewController {
+    
+    fileprivate lazy var pageTitleView: PageTitleView = {
+        let navigationMaxY: CGFloat = (navigationController?.navigationBar.frame.maxY) ?? 88
+        let titleFrame = CGRect(x: 0, y: navigationMaxY, width: kScreenW, height: kTitleViewH)
+        let titles = ["推荐", "游戏", "娱乐", "趣玩"]
+        let titleView = PageTitleView(frame: titleFrame, titles: titles)
+        return titleView
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         setupUI()
     }
-    
 }
 
 
 extension HomeViewController{
     fileprivate func setupUI(){
         setupNavigationBar()
+        
+        view.addSubview(pageTitleView)
+        
     }
     
     fileprivate func setupNavigationBar(){
--- live-streaming-demo/live-streaming-demo/Classes/Home/View/PageTitleView.swift
+++ live-streaming-demo/live-streaming-demo/Classes/Home/View/PageTitleView.swift
@@ -8,16 +8,28 @@
 
 import UIKit
 
+private let kScrollLineH: CGFloat = 2
+private let kNormalColor: (CGFloat, CGFloat, CGFloat) = (85, 85, 85)
+private let kSelectColor: (CGFloat, CGFloat, CGFloat) = (255, 128, 0)
+
 class PageTitleView: UIView {
     
     fileprivate var titles: [String]
+    fileprivate var currentIndex: Int = 0
+    fileprivate lazy var titleLabels: [UILabel] = [UILabel]()
     
     fileprivate lazy var scrollView: UIScrollView = {
        let sc = UIScrollView()
 //        sc.showsVerticalScrollIndicator = false
-//        sc.scrollsToTop = false
-//        sc.bounces = false
+        sc.scrollsToTop = false
+        sc.bounces = false
         return sc
+    }()
+    
+    fileprivate lazy var scrollLine: UIView = {
+       let scrollLine = UIView()
+        scrollLine.backgroundColor = UIColor.orange
+        return scrollLine
     }()
 
     init(frame: CGRect, titles: [String]) {
@@ -34,6 +46,64 @@ class PageTitleView: UIView {
 
 extension PageTitleView{
     fileprivate func setupUI(){
+//    self.backgroundColor = UIColor.red
+        addSubview(scrollView)
+        scrollView.frame = bounds
+        
+        setupTitleLabels()
+        setupBottomLineAndScrollLine()
+    }
     
+    fileprivate func setupTitleLabels(){
+        let labelW: CGFloat = frame.width/CGFloat(titles.count)
+        let labelH: CGFloat = frame.height - kScrollLineH
+        let labelY: CGFloat = 0
+        for (index, title) in titles.enumerated() {
+            let label = UILabel()
+            label.text = title
+            label.tag = index
+            label.font = UIFont.systemFont(ofSize: 16.0)
+            label.textAlignment = .center
+            let labelX: CGFloat = labelW * CGFloat(label.tag)
+            label.frame = CGRect(x: labelX, y: labelY, width: labelW, height: labelH)
+            scrollView.addSubview(label)
+            titleLabels.append(label)
+            
+            label.isUserInteractionEnabled = true
+            let tapGes = UITapGestureRecognizer(target: self, action: #selector(self.titleLabelClick(_:)))
+            label.addGestureRecognizer(tapGes)
+        }
+    }
+    
+    fileprivate func setupBottomLineAndScrollLine(){
+        let bottomLine = UIView()
+        bottomLine.backgroundColor = UIColor.lightGray
+        let lineH: CGFloat = 0.5
+        bottomLine.frame = CGRect(x: 0, y: frame.height - lineH, width: frame.width, height: lineH)
+        addSubview(bottomLine)
+        
+        guard let firstLabel = titleLabels.first else { return }
+        firstLabel.textColor = UIColor(r: kSelectColor.0, g: kSelectColor.1, b: kSelectColor.2)
+        scrollView.addSubview(scrollLine)
+        scrollLine.frame = CGRect(x: 0, y: frame.height - kScrollLineH, width: firstLabel.frame.width, height: kScrollLineH)
+    }
+}
+
+
+extension PageTitleView {
+    @objc fileprivate func titleLabelClick(_ tapGes : UITapGestureRecognizer) {
+        guard let currentLabel = tapGes.view as? UILabel else{ return }
+        if currentLabel.tag == currentIndex { return }  //如果重复点击同一个label 直接返回
+        
+        let oldLabel = titleLabels[currentIndex]
+        currentLabel.textColor = UIColor(r: kSelectColor.0, g: kSelectColor.1, b: kSelectColor.2)
+        oldLabel.textColor = UIColor(r: kNormalColor.0, g: kNormalColor.1, b: kNormalColor.2)
+        currentIndex = currentLabel.tag
+        
+        let scrollLineX = CGFloat(currentIndex) * scrollLine.frame.width
+        UIView.animate(withDuration: 0.15) {
+            self.scrollLine.frame.origin.x = scrollLineX
+        }
+        
     }
 }
